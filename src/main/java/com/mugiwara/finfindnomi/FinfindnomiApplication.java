f669ad52386eeb5c5d68b22b33d1bcb60cfb7b59
donovanbrun <donovanbrun@gmail.com>
2022-11-11 22:45:11 +0100

    initialise project

---
+++ src/main/java/com/mugiwara/finfindnomi/FinfindnomiApplication.java
@@ -0,0 +1,13 @@
+package com.mugiwara.finfindnomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfindnomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfindnomiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugiwara/finfindnomi/FinfindnomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugiwara.finfindnomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinfindnomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
